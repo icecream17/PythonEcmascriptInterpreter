1db6e8355888084b5f0df4e7051cf62c1262f1b5
steven nguyen <icecream17.github@gmail.com>
2021-04-19 10:20:55 -0500

    change version [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='js_interpreter',
-   version='0.2.0',
+   version='0.3.0.2',
    description='A javascript interpreter made in python',
    url='https://github.com/icecream17/python-javascript-interpreter',
    author='Steven Nguyen (icecream17)',
